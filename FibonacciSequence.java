a5783c10649266d6c68d315f032182c7045a783a
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-05-11 22:22:12 +0200

    next commit

---
+++ FibonacciSequence.java
@@ -0,0 +1,11 @@
+package sda.java9.wpj.advanced.proc;
+
+public class FibonacciSequence
+{
+	public static void main (String [] args)
+	{
+	
+	}
+	
+	
+}
--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,25 +2,15 @@ package sda.java9.wpj.advanced.proc;
 
 public class FizzBuzz
 {
-	public static void main (String [] args)
-	{
-		
-	    for (int i = 1; i < 101; i++) 
-		{
-            if ( i % 15 == 0)
-			{
+	public static void main (String [] args){
+		for (int i = 1; i < 101; i++){
+			if ( i % 15 == 0){
 				System.out.println("FizzBuzz");
-			}
-			else if (i % 3 == 0)
-			{
+			}else if (i % 3 == 0){
 				System.out.println("Fizz");
-			}
-			else if (i % 5 == 0)
-			{
+			}else if (i % 5 == 0){
 				System.out.println("Buzz");
-			}
-			else
-			{
+			}else{
 				System.out.println(i);
 			}
 		}
